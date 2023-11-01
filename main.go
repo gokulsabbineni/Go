f73cc28e971f7cfea4a6c428a96f5c26d2cac519
gokulsai121 <gs66@njit.edu>
2023-11-01 11:08:24 -0400

    custom package

--- main.go
+++ main.go
@@ -2,20 +2,49 @@ package main
 
 import (
 	"fmt"
+	"myproject/simplecalc"
 )
 
-func main() {
-	fmt.Println("HELLO!")
-	a, b := 12.0, 3.0
-	fmt.Println(a, b)
+const Hello = "hello world"
 
+func main() {
+
+	var hello int = 64
+
+	// a := "string"
+
+	a, b := 12, 12
+	fmt.Println("Hello")
+	// fmt.Println(simplecalc.Add(a, b))
+	fmt.Println(simplecalc.Sub(a, b))
+	fmt.Println(Hello)
+	fmt.Println(hello)
 }
 
-// func sayHello() {
-// 	fmt.Println("Hello!")
-// }
+// datatypes in go:
 
-// func main() {
-// 	go sayHello()
-// 	fmt.Println("Main function")
-// }
+// 1. Boolean Type: bool    var a bool a =
+// 2. Numeric Types: Int, Float, Complex Numbers
+
+// Int : Signed Integer and Unsigned Integer    -1    1   1
+
+// Int, int8, int16, int64
+
+// Uint, uint8,uint16,uint64
+
+// Float: float32 and float64
+
+// Complex Number : 102 + 5i complex64 and comple128
+
+// 3. string type : string
+
+// 4. dervied types:
+
+// Default Value
+
+// Constants
+
+// Int = 0
+// string = ""
+// bool = false
+// float64 = 0.0
---
+++ simplecalc/calc.go
@@ -0,0 +1,9 @@
+package simplecalc
+
+func Add(a, b float64) float64 {
+	return a + b
+}
+
+func Sub(a, b int) int {
+	return a - b
+}
